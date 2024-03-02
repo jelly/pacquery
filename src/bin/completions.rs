5069f97d6144432a49ebe1b64b16966e3661bf0e
Jelle van der Waa <jelle@archlinux.org>
2024-03-02 20:21:39 +0100

    Generate shell completion

---
+++ src/bin/completions.rs
@@ -0,0 +1,20 @@
+use clap::{CommandFactory, ValueEnum};
+use clap_complete::Shell;
+use std::env;
+
+use pacquery::args::Args;
+
+fn main() -> anyhow::Result<()> {
+    //  https://doc.rust-lang.org/cargo/reference/environment-variables.html
+    let out_dir = env::var_os("OUT_DIR").expect("out dir not set");
+    for variant in Shell::value_variants() {
+        clap_complete::generate_to(
+            *variant,
+            &mut Args::command(),
+            env!("CARGO_PKG_NAME"),
+            &out_dir,
+        )?;
+    }
+    println!("completion scripts generated in {:?}", out_dir);
+    Ok(())
+}
